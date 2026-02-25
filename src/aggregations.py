c5de1e9706a463823420e2083a14dcdf7de00c34
Jayesh Suryawanshi <jps25102001@gmail.com>
2026-02-25 15:45:37 +0530

    Added aggregations, fallback initial structure to src, created a config yaml file, and a requirements file

---
+++ src/aggregations.py
@@ -0,0 +1,4 @@
+AGGREGATION_REGISTRY = {
+    "mean": mean_aggregation,
+    "sum": sum_aggregation,
+}
---
+++ src/fallback.py
@@ -0,0 +1,4 @@
+FALLBACK_REGISTRY = {
+    "nan_if_no_valid": nan_fallback,
+    "propagate_special": propagate_special_fallback,
+}
